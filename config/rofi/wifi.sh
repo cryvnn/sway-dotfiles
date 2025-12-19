8f9c93220e29b9df549643201e127f51d7770e44
cryvnn <cryvnn@protonmail.com>
2025-12-19 20:28:41 +0100

    small changes

--- config/rofi/wifi.sh
+++ config/rofi/wifi.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
-
 notify-send "Getting list of available Wi-Fi networks..."
-# Get a list of available wifi connections and morph it into a nice-looking list
+
 wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")
 
 connected=$(nmcli -fields WIFI g)
@@ -11,9 +9,8 @@ elif [[ "$connected" =~ "disabled" ]]; then
 	toggle="󰖩  Enable Wi-Fi"
 fi
 
-# Use rofi to select wifi network
 chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " )
-# Get name of connection
+
 read -r chosen_id <<< "${chosen_network:3}"
 
 if [ "$chosen_network" = "" ]; then
@@ -23,9 +20,8 @@ elif [ "$chosen_network" = "󰖩  Enable Wi-Fi" ]; then
 elif [ "$chosen_network" = "󰖪  Disable Wi-Fi" ]; then
 	nmcli radio wifi off
 else
-	# Message to show when connection is activated successfully
   	success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
-	# Get saved connections
+
 	saved_connections=$(nmcli -g NAME connection)
 	if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
 		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
